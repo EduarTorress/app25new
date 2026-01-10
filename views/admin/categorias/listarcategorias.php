2dd6519022cb5fa83e0aebe67df35388c16dd846
Eduartch <eduarenriquet@gmail.com>
2026-01-10 09:40:30 -0500

    2026-01-10

--- views/admin/categorias/listarcategorias.php
+++ views/admin/categorias/listarcategorias.php
@@ -11,8 +11,8 @@
         <?php foreach ($lista['lista']['items'] as $item) : ?>
             <tr>
                 <td><?php echo $item['idcat'] ?></td>
-                <td><?php echo $item['dcat'] ?></td>
-                <td><?php echo $item['totalproductos'] ?></td>
+                <td class="text-center"><?php echo $item['dcat'] ?></td>
+                <td class="text-end"><?php echo $item['totalproductos'] ?></td>
                 <td class="text-center" id="iniciar">
                     <?php
                     $parametro1 = $item['idcat'];
