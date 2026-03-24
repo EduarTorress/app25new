52c5b14e19f99d35abe5b50013c4eb015d0731e7
Eduartch <eduarenriquet@gmail.com>
2026-03-24 11:01:48 -0500

    2026-03-24

--- core/Clases/Imprimir.php
+++ core/Clases/Imprimir.php
@@ -1985,7 +1985,7 @@ class Imprimir
 
         $pdf->setx(5);
         $pdf->Cell(50, 4, 'EGRESOS: ', 0, 0, 'L');
-        $pdf->Cell(15, 4, 'S/ ' . (Round($this->egresos, 2)), 0, 1, 'L');
+        $pdf->Cell(15, 4, '- S/ ' . (Round($this->egresos, 2)), 0, 1, 'L');
         $pdf->ln();
 
         $pdf->SetFont('Arial', 'B', 9);
