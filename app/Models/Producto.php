da18ee242ddd50b155d602ab657f8b0c48a7004a
Eduartch <eduarenriquet@gmail.com>
2026-08-19 18:44:25 -0500

    2026-08-20

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -616,7 +616,7 @@ class Producto extends Modelo
                         "epta_cant" => (isset($row['epta_cant'])) ? $row['epta_cant'] : '1',
                         "epta_idep" => (isset($row['epta_idep'])) ? $row['epta_idep'] : '0',
                         "prod_tigv" => (empty($row['prod_tigv'])) ? 1 : $row['prod_tigv'],
-                        "prod_dola" => (empty($row['prod_dola'])) ? session()->get('gene_dola') : $row['prod_dola']
+                        "prod_dola" => (empty($row['prod_dola']) ? session()->get('gene_dola') : $row['prod_dola'])
                         #falta sacar la formula del costo neto
                     );
                     array_push($lista["items"], $item);
--- views/components/re_listaproductos.php
+++ views/components/re_listaproductos.php
@@ -80,7 +80,7 @@
                             $prod_uti2 = $items[0]['prod_uti2'];
                             $prod_uti3 = $items[0]['prod_uti3'];
                             $prod_tigv = $items[0]['prod_tigv'];
-                            $prod_dola = $items[0]['prod_dola'];
+                            $prod_dola = (empty($items[0]['prod_dola']) ? '0' : $items[0]['prod_dola']);
                             $txtcoda1 = str_replace("'", '"', $items[0]['txtcoda1']);
                             $j = 0;
                             $presentaciones = [];
