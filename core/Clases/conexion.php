0b854689f553694a20dec715dc4b1cec4a244484
Eduartch <eduarenriquet@gmail.com>
2026-08-26 11:23:32 -0500

    2026-08-26

--- core/Clases/conexion.php
+++ core/Clases/conexion.php
@@ -13,145 +13,185 @@ class conexion
     private $nombre_de_base = '';
     private $usuario = '';
     private $contrasena = '';
-    private $conexion;
-    private $empresa;
+
+    private $pdo = null;
+
     public $lastinsertid;
-    protected $pdo;
+
     public function __construct()
     {
         $config = require $_ENV['DIR_ROOT'] . "/config/app.php";
+
         $this->nombre_de_base = $config['database']['database'];
-        $this->host = $config['database']['host'];
-        $this->usuario = $config['database']['username'];
-        $this->contrasena = $config['database']['password'];
+        $this->host           = $config['database']['host'];
+        $this->usuario       = $config['database']['username'];
+        $this->contrasena    = $config['database']['password'];
     }
+
     public function conectar()
     {
         try {
-            $connection = $this->tipo_de_base . ":host=" . $this->host . ";dbname=" . $this->nombre_de_base . ";charset=utf8";
+            // Si ya existe una conexión, reutilizarla
+            if ($this->pdo instanceof PDO) {
+                return $this->pdo;
+            }
+
+            $connection =
+                $this->tipo_de_base .
+                ":host=" . $this->host .
+                ";dbname=" . $this->nombre_de_base .
+                ";charset=utf8mb4";
+
             $options = [
                 PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_EMULATE_PREPARES   => false,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             ];
-            $this->pdo = new PDO($connection, $this->usuario, $this->contrasena);
+
+            $this->pdo = new PDO(
+                $connection,
+                $this->usuario,
+                $this->contrasena,
+                $options
+            );
+
             return $this->pdo;
         } catch (PDOException $e) {
-            print_r('Error de Conexión con la base de Datos');
+
+            throw new Exception(
+                'Error de conexión con la base de datos: ' .
+                    $e->getMessage()
+            );
         }
     }
+
     public function getData($sql)
     {
-        $data = array();
-        $result = $this->conexion->query($sql);
-        $error = $this->conexion->errorInfo();
-        if ($error[0] === "00000") {
-            $result->execute();
-            if ($result->rowCount() > 0) {
-                while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-                    array_push($data, $row);
-                }
-            }
-        } else {
-            throw new Exception($error[2]);
-        }
-        return $data;
+        $stmt = $this->conectar()->query($sql);
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
+
+    public function getDataSingle($sql)
+    {
+        $stmt = $this->conectar()->query($sql);
+
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $data ?: null;
+    }
+
+    public function getDataSingleProp($sql, $prop)
+    {
+        $data = $this->getDataSingle($sql);
+
+        return $data[$prop] ?? null;
+    }
+
     public function numRows($sql)
     {
-        $result = $this->conexion->query($sql);
-        $error = $this->conexion->errorInfo();
-        if ($error[0] === "00000") {
-            $result->execute();
-            return $result->rowCount();
-        } else {
-            throw new Exception($error[2]);
-        }
+        $stmt = $this->conectar()->query($sql);
+
+        return $stmt->rowCount();
     }
-    function getDataSingle($sql)
-    {
-        $result = $this->conexion->query($sql);
-        $error = $this->conexion->errorInfo();
-        if ($error[0] === "00000") {
-            $result->execute();
-            if ($result->rowCount() > 0) {
-                return $result->fetch(PDO::FETCH_ASSOC);
-            }
-        } else {
-            throw new Exception($error[2]);
-        }
-        return null;
-    }
-    function getDataSingleProp($sql, $prop)
-    {
-        $result = $this->conexion->query($sql);
-        $error = $this->conexion->errorInfo();
-        if ($error[0] === "00000") {
-            $result->execute();
-            if ($result->rowCount() > 0) {
-                $data = $result->fetch(PDO::FETCH_ASSOC);
-                return $data[$prop];
-            }
-        } else {
-            throw new Exception($error[2]);
-        }
-        return null;
-    }
-    function execute($query = '', $return_rows = 0, $array_valores = array(), $array_tipos = array())
-    {
-        $this->_pdoStat = $this->_pdo->prepare($query);
+
+    public function execute(
+        $query = '',
+        $return_rows = 0,
+        $array_valores = [],
+        $array_tipos = []
+    ) {
+        $stmt = $this->conectar()->prepare($query);
+
         foreach ($array_valores as $posicion => &$valor) {
-            $tipo_var = 'STR' == $array_tipos[$posicion] ? PDO::PARAM_STR : PDO::PARAM_INT;
-            $this->_pdoStat->bindParam($posicion + 1, $valor, $tipo_var);
+
+            $tipo_var =
+                isset($array_tipos[$posicion]) &&
+                $array_tipos[$posicion] === 'STR'
+                ? PDO::PARAM_STR
+                : PDO::PARAM_INT;
+
+            $stmt->bindValue(
+                $posicion + 1,
+                $valor,
+                $tipo_var
+            );
         }
-        $result = $this->_pdoStat->execute();
-        if (0 < $return_rows && $result) {
-            return $return_rows == 2 ? $this->_pdoStat->fetch() : $this->_pdoStat->fetchAll();
+
+        $result = $stmt->execute();
+
+        if ($return_rows > 0 && $result) {
+
+            return $return_rows == 2
+                ? $stmt->fetch(PDO::FETCH_ASSOC)
+                : $stmt->fetchAll(PDO::FETCH_ASSOC);
         }
+
         return $result;
     }
-    function executeInstruction($sql)
+
+    public function executeInstruction($sql)
     {
-        $result = $this->conexion->query($sql);
-        $error = $this->conexion->errorInfo();
-        if ($error[0] === "00000") {
-            $result->execute();
-            return $result->rowCount() > 0;
-        } else {
-            throw new Exception($error[2]);
-        }
+        $stmt = $this->conectar()->query($sql);
+
+        return $stmt->rowCount() > 0;
     }
-    function close()
+
+    public function getLastId()
     {
-        $this->pdo = null;
+        return $this->conectar()->lastInsertId();
     }
-    function getLastId()
-    {
-        return $this->conexion->lastInsertId();
-    }
+
     public function startTransaction()
     {
-        $this->conectar()->beginTransaction();
-        if ($this->conectar()->errorCode() != '00000') {
+        try {
+
+            $pdo = $this->conectar();
+
+            if (!$pdo->inTransaction()) {
+                $pdo->beginTransaction();
+            }
+
+            return true;
+        } catch (PDOException $e) {
+
             return false;
         }
-        return true;
     }
+
     public function insertTransaction($sql, $data)
     {
-        $stmt = $this->pdo->prepare($sql);
+        $stmt = $this->conectar()->prepare($sql);
+
         $stmt->execute($data);
-        $this->lastinsertid = $this->pdo->lastInsertId();
+
+        $this->lastinsertid =
+            $this->conectar()->lastInsertId();
+
+        return true;
     }
+
     public function submitTransaction()
     {
         try {
-            $this->pdo->commit();
+
+            if ($this->conectar()->inTransaction()) {
+                $this->conectar()->commit();
+            }
+
+            return true;
         } catch (PDOException $e) {
-            $this->pdo->rollBack();
+
+            if ($this->conectar()->inTransaction()) {
+                $this->conectar()->rollBack();
+            }
+
             return false;
         }
-        return true;
+    }
+
+    public function close()
+    {
+        $this->pdo = null;
     }
 }
-/*https://www.digitalocean.com/community/tutorials/how-to-use-the-pdo-php-extension-to-perform-mysql-transactions-in-php-on-ubuntu-18-04-es*/
-/*https://es.stackoverflow.com/questions/8197/m%C3%A9todo-din%C3%A1mico-para-realizar-transacciones-de-actualizaci%C3%B3n-con-pdo*/
